f8d2f79c7bfeaf7719065f3b91f21329ca405b77
zhang-yi-fei <38426541+zhang-yi-fei@users.noreply.github.com>
2018-06-12 06:30:55 +0800

    Add results

--- project_1/src/main/java/de/fraunhofer/iais/kd/bda/spark/CloseToMark.java
+++ project_1/src/main/java/de/fraunhofer/iais/kd/bda/spark/CloseToMark.java
@@ -11,6 +11,18 @@ public class CloseToMark {
 	private static final double distanceLimit = 0.85;
 	private static final String targetArt = "Mark Knopfler";
 
+	/**
+	 * Artists that are closer than 0.85 to Mark Knopfler:
+	 * 
+	 * (Jeff Beck,0.813953488372093) (Zz Top,0.8082191780821918) (Wishbone
+	 * Ash,0.8378378378378378) (Stevie Ray Vaughan And Double Trouble,0.84) (Gary
+	 * Moore,0.8085106382978724) (Mark Knopfler,0.0) (Camel,0.8409090909090909)
+	 * (Grand Funk Railroad,0.8055555555555556) (Rainbow,0.847457627118644) (Buena
+	 * Vista Social Club,0.8461538461538461) (B.B. King,0.8)
+	 * (Tesla,0.8378378378378378) (Muddy Waters,0.8245614035087719)
+	 * 
+	 */
+
 	public static void main(String[] args) {
 		String inputFile = "resources/last-fm-sample1000000.tsv";
 		String appName = "CloseToMark";
